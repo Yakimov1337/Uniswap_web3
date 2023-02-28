ce62ac0236c9dfa5edcf22941331244a5708f955
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-02-28 20:50:01 +0200

    chain id and router address added (env)

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = process.env.NEXT_PUBLIC_ROUTER_ADDRESS; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: process.env.NEXT_PUBLIC_CHAIN_ID,
   },
 };

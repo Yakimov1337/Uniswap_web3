aa369f494793c80bc6f3f6e969ff96b60756f11d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-03-03 03:09:54 +0200

    REACT_APP env variables

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,9 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = process.env.NEXT_PUBLIC_ROUTER_ADDRESS; 
-
+export const ROUTER_ADDRESS = process.env.REACT_APP_ROUTER_ADDRESS; 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: process.env.NEXT_PUBLIC_CHAIN_ID,
+    [Goerli.chainId]: process.env.REACT_APP_CHAIN_ID,
   },
 };
